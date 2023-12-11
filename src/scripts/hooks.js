67fa06616a3a7a3f543f87a4c2c40af0911936b5
Shadows_eye <71041904+shadows-eye@users.noreply.github.com>
2023-12-11 10:48:31 +0100

    update_html_fixes_css
    This now completes the CSS for the monitor to function on opening and can be repositioned and resized, this is fully scaleable. Status not in yet

--- src/scripts/hooks.js
+++ src/scripts/hooks.js
@@ -1,9 +1,25 @@
 import BioMon from "./biomon.js"
 import Settings from "./settings.js"
 
-Hooks.on("ready", () => {
+  Hooks.on("ready", () => {
     Settings.addAllSettings();
-    
-    ui.BIOMONITOR = new BioMon();
-    ui.BIOMONITOR.render(true);
   });
+
+ Hooks.on('getSceneControlButtons', (controls) => {
+   const tokenControls = controls.find((c) => c.name === 'token');
+
+   tokenControls.tools.push({
+       name: 'sr5-biomon',
+       title: 'BioMon',
+       icon: 'fas fa-heartbeat',
+       button: true
+   });
+ });
+
+ Hooks.on('renderSceneControls', (controls, html) => {
+    html.find('[data-tool="sr5-biomon"]').on('click', (event) => {
+         event.preventDefault();
+         ui.BIOMONITOR = new BioMon();
+         ui.BIOMONITOR.render(true);
+      });
+ });
