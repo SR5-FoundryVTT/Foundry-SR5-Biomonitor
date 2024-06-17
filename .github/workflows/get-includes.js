263d9883ef98d94ce76f649cb981c2e010a796fa
Stexinator <hurricane90@gmx.de>
2024-06-17 20:59:33 +0200

    add v12 compatibility

--- .github/workflows/get-includes.js
+++ .github/workflows/get-includes.js
@@ -1,2 +1,2 @@
 var fs = require('fs');
-console.log(JSON.parse(fs.readFileSync('src/module.json', 'utf8')).includes.join(" "));
+console.log(JSON.parse(fs.readFileSync('src/includes.json', 'utf8')).includes.join(" "));
